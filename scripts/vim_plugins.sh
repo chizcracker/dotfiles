e65e73d4450e71c9fc7817f7da8b83bc212a27e8
JEUNG WON KIM <jeungwok@JEUNGui-MacBook-Pro.local>
2014-08-14 23:55:51 -0500

    install path fix

--- scripts/vim_plugins.sh
+++ scripts/vim_plugins.sh
@@ -5,12 +5,12 @@
 # curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 TDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
-PP='/.vimrc/bundle'
+PP='/.vim/bundle'
+mkdir -p $HOME$PP
 
 trap "{ cd - ; rm -rf $TDIR; exit 255; }" SIGINT
 
 cd $TDIR
-mkdir -p $HOME$PP
 git clone git://github.com/vim-scripts/VimClojure.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone git://github.com/ntpeters/vim-better-whitespace.git
