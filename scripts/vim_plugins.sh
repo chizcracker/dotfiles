5790633e0eb3d690472b714df91facc1427445dd
Jeung Won Kim <chizcracker@gmail.com>
2014-08-28 22:15:54 -0500

    added supertab && ctrlp

--- scripts/vim_plugins.sh
+++ scripts/vim_plugins.sh
@@ -20,6 +20,8 @@ git clone https://github.com/altercation/vim-colors-solarized.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone git://github.com/ntpeters/vim-better-whitespace.git
 git clone https://github.com/mileszs/ack.vim.git
+git clone https://github.com/ervandew/supertab.git
+git clone https://github.com/kien/ctrlp.vim.git
 
 # clojure
 git clone git://github.com/vim-scripts/VimClojure.git
